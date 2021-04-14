9767acb06185845847e354ee12fa8f6e20bf84eb
Greko2017 <gregory.goufan@gmail.com>
2021-04-14 09:18:05 +0100

    fix problem on loding csv file

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,78 +1,85 @@
-
-import React, { Component } from 'react'
+import React, { Fragment, useEffect, useState } from 'react';
 import Message from './Message';
 import Progress from './Progress';
 import axios from 'axios';
-import TableList from './TableList'
+import TableList from './TableList';
 
-class FileUpload extends Component {
-  state = {
-    file:'',
-    filename:'Choose File',
-    uploadedFile:{},
-    message:'',
-    uploadPercentage:0,
-    tableData:[],
-  }
-  render() {
-    const onChange = e => {
-      this.setState({file: e.target.files[0]});
-      this.setState({filename:e.target.files[0].name});
-    };
-    
+const FileUpload = () => {
+
+  const [file, setFile] = useState('');
+  const [filename, setFilename] = useState('Choose File');
+  const [uploadedFile, setUploadedFile] = useState({});
+  const [message, setMessage] = useState('');
+  const [uploadPercentage, setUploadPercentage] = useState(0);
+  
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    // Met à jour le titre du document via l’API du navigateur
+    // console.log(`In FileUpload`,tableData)
+  });
+
+  const onChange = e => {
+    e.preventDefault();
+    setFile(e.target.files[0]);
+    setFilename(e.target.files[0].name);
+  };
 
   const onSubmit = e => {
     e.preventDefault();
+    console.log('onSubmit :>> ', file);
     const formData = new FormData();
-    formData.append('file', this.state.file);
-
+    formData.append('file', file);
     try {
        axios.post('/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         },
         onUploadProgress: progressEvent => {
-          this.setState({uploadPercentage:
+          setUploadPercentage(
             parseInt(
               Math.round((progressEvent.loaded * 100) / progressEvent.total)
-            )});
+            )
+          );
 
           // Clear percentage
-          setTimeout(() => this.setState({uploadPercentage:0}), 10000);
+          setTimeout(() => setUploadPercentage(0), 10000);
         }
       }).then(res =>{
 
         const { fileName, filePath,CsvDataList } = res.data;
-        console.log(`--- In FileUpload In CsvDataList`,)
-        CsvDataList.map((element) => {
+        // console.log(`--- In FileUpload In CsvDataList:>>`,CsvDataList)
+        CsvDataList.forEach((element) => {
           let tmp = {
-            gl_date:element['"Gl_Date"'],
+            gl_date:element["Gl_Date"],
             abr:element['Abr'],
             parent_acc_nature_view:element['Parent Account for Nature View'],
             currency:element['Currency'],
             actual_amount:element['Actual\nAmount']
           }
           // console.log('tmp :>> ', tmp);
-
-          this.setState({tableData:[...this.state.tableData, element]});
+          
+          setTableData(tableData => [...tableData,tmp])
         });
-        this.setState({uploadedFile:{ fileName:fileName, filePath:filePath}});
+        setUploadedFile({ fileName, filePath });
   
-        this.setState({message:'File Uploaded'});
+        setMessage('File Uploaded');
 
       })
 
     } catch (err) {
       if (err.response.status === 500) {
-        this.setState({message:'There was a problem with the server'});
+        setMessage('There was a problem with the server');
       } else {
-        this.setState({message:err.response.data.msg});
+        setMessage(err.response.data.msg);
       }
     }
+
+
   };
-    return (
-    <div>
-      {this.state.message ? <Message msg={this.state.message} /> : null}
+  return (
+    <Fragment>
+      {message ? <Message msg={message} /> : null}
       <form onSubmit={onSubmit}>
         <div className='custom-file mb-4'>
           <input
@@ -82,11 +89,11 @@ class FileUpload extends Component {
             onChange={onChange}
           />
           <label className='custom-file-label' htmlFor='customFile'>
-            {this.state.filename}
+            {filename}
           </label>
         </div>
 
-        <Progress percentage={this.state.uploadPercentage} />
+        <Progress percentage={uploadPercentage} />
 
         <input
           type='submit'
@@ -94,21 +101,21 @@ class FileUpload extends Component {
           className='btn btn-primary btn-block mt-4'
         />
       </form>
-      {this.state.uploadedFile ? (
+      {uploadedFile ? (
         <div className='row mt-5'>
           <div className='col-md-6 m-auto'>
-            <h3 className='text-center'>{this.state.uploadedFile.fileName}</h3>
-            <img style={{ width: '100%' }} src={this.state.uploadedFile.filePath} alt='' />
+            <h3 className='text-center'>{uploadedFile.fileName}</h3>
+            <img style={{ width: '100%' }} src={uploadedFile.filePath} alt='' />
           </div>
         </div>
       ) : null}
-      {/* {this.state.tableData.length>=1?( */}
-      <TableList tableData={this.state.tableData} />
-      {/* ):<h6>Please import a sheet</h6>} */}
+
+      {/* {tableData.length>=1?( */}
+      <TableList tableData={tableData} />
+      {/* ):(<h6>Please import a sheet</h6>)} */}
       
-    </div>
-    )
-  }
-}
+    </Fragment>
+  );
+};
 
 export default FileUpload;
